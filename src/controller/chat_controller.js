1cfe6a888554d5110368508b0f8de497d30f5afc
digitkhrisnaa <digital.khrisna@gmail.com>
2016-10-05 10:40:03 +0700

    Remove parameter partnerId for send chat API

--- src/controller/chat_controller.js
+++ src/controller/chat_controller.js
@@ -20,20 +20,19 @@ var push = new fcm_module();
 * @desc: Send message to unique chat room use firebase cloud messaging with spesific token
 */
 module.exports.sendMessage = function(req, res){
-    if(!req.body.roomid || !req.body.fbid || !req.body.partnerid) return res.json({status:400, message:"Bad Request"});
+    if(!req.body.roomid || !req.body.fbid) return res.json({status:400, message:"Bad Request"});
 
-    console.log(req.body.roomid);
     Chat.findOne({room_ids:req.body.roomid}).populate('participant').exec(function(err, data){
         if(err) return res.json({status:500, message:"Internal server error " + err.toString()});
         if(data == null) return res.json({status:204, message:"Rooms not found"});
         var dataPush = {
-            sender:search(data.participant, req.body.fbid),
+            sender:search(data.participant, req.body.fbid, 1), //enum 1 is for looking sender collection
             body:req.body.message,
             timeStamp: new Date()
         }
         data.message.push(dataPush);
         data.save();
-        var partnerToken = search(data.participant, req.body.partnerid)
+        var partnerToken = search(data.participant, req.body.fbid, 2) //enum 2 is for looking partner collection
         var pushProperty = {
             token_firebase:partnerToken.token_firebase,
             message:req.body.message
@@ -75,11 +74,17 @@ module.exports.retrieveMessage = function(req, res){
 * @return: List
 * @desc: search user in collection
 */
-function search(collection, fbid){
+function search(collection, fbid, searchMethod){
     var op = 0;
     for (var i=0; i < collection.length; i++) {
-        if (collection[i].fbid === fbid) {
-            return collection[i];
+        if(searchMethod === 1){ //looking for current fbid
+            if (collection[i].fbid === fbid) {
+                return collection[i];
+            }
+        }else{ //looking for partner fbid
+            if(collection[i].fbid !== fbid){
+                return collection[i];
+            }
         }
     }
 }
